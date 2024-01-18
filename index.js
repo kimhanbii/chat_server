de3e04cf4f42b91b3424a95556e6753f5c6a8cfe
khb <stellar0109@naver.com>
2024-01-18 17:02:42 +0900

    chat_server

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+
+const http = require('http').createServer(app)
+const io = require('socket.io')(http, {cors:{origin:"*"}})
+
+let userInfo = []
+
+io.on('connection', (socket)=>{
+    console.log("user connection!!")
+
+    socket.on('disconnect', ()=>{
+        const user = userInfo.find(c=>c.id == socket.id)
+        socket.broadcast.emit('msg',{level:"sys", msg:user.input_ref + "님이 퇴장하였습니다"})
+    })
+
+    socket.on('login', (input_ref)=>{
+        const info = {
+            input_ref:input_ref,
+            id:socket.id
+        }
+        userInfo.push(info)
+        io.emit('msg',{level:"sys", msg:input_ref + " 님이 입장하였습니다"})
+    })
+    
+    socket.on('send', (msg1)=>{
+        // io.emit('msg',{msg:msg1})
+        socket.broadcast.emit('msg',{level:"", msg:msg1})
+        // console.log(input_ref + " 님이 입장하였습니다")
+    })
+})
+
+http.listen(process.env.PROT || 3001, ()=>{
+    console.log("chat")
+})
